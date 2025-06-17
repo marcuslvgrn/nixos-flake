3a023030ff98685e6805d3870c8a47e1a10a8dec
marcuslvgrn <marcuslvgrn@gmail.com>
2025-06-17 18:30:25 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Marcus nix config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      # FIXME replace with your hostname
+      nixosX360 = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [./hosts/nixosX360/configuration.nix];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+   # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      lovgren = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        # > Our main home-manager configuration file <
+        modules = [./home-manager/home.nix];
+      };
+    };
+  };
+}
---
+++ hosts/nixosX360/configuration.nix
@@ -0,0 +1,229 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ inputs, config, lib, pkgs, ... }:
+
+let
+  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+in
+{
+  imports =
+    [ 
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      # Home Manager
+      "${home-manager}/nixos"
+      ./gnome.nix
+#      ./xfce.nix
+      ./boot-loader.nix
+      ./networking.nix
+#      ./vmware.nix
+      ./soap-nix.nix
+    ];
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Set your time zone.
+  time.timeZone = "Europe/Stockholm";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "sv_SE.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+  #  keyMap = "us";
+  #  useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  # Configure keymap in X11
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.lovgren = {
+     isNormalUser = true;
+     description = "Marcus Lövgren";
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+       tree
+     ];
+     shell = pkgs.bash;
+     home = "/home/lovgren";
+   };
+
+   # Call commands and interactive bash start
+   # Commands are separated by \n
+   programs.bash.interactiveShellInit = "LANG=en_US.UTF-8\n neofetch\n";
+
+  #home manager
+  home-manager.users.root = { pkgs, ... }: {
+    #GIT
+    programs.git = {
+      enable = true;
+      userEmail = "marcuslvgrn@gmail.com";
+      userName = "marcuslvgrn";
+    };
+    programs.bash = {
+      enable = true;
+      shellAliases = {
+        ll = "ls -la";
+         l = "ls -alh";
+        ls = "ls --color=tty";
+      };
+    };
+    home.stateVersion = "25.05";
+    programs.home-manager.enable = true;
+  };
+  
+  home-manager.users.lovgren = { pkgs, ... }: {
+    home.sessionVariables = {
+      LANG = "sv_SE.UTF-8";
+    };
+    home.username = "lovgren";
+    home.homeDirectory = "/home/lovgren";
+    home.packages = with pkgs; [ 
+      gnomeExtensions.dash-to-dock
+    ];
+    programs.bash = {
+      enable = true;
+      shellAliases = {
+        ll = "ls -la";
+         l = "ls -alh";
+        ls = "ls --color=tty";
+      };
+    };
+    
+    #GIT
+    programs.git = {
+      enable = true;
+      userEmail = "marcuslvgrn@gmail.com";
+      userName = "marcuslvgrn";
+    };
+
+    dconf = {
+      enable = true;
+      settings."org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = with pkgs.gnomeExtensions; [
+          dash-to-dock.extensionUuid
+        ];
+      };
+    };
+    # The state version is required and should stay at the version you
+    # originally installed.
+    home.stateVersion = "25.05";
+    programs.home-manager.enable = true;
+  };
+
+  programs = {
+    #FIREFOX
+    firefox = {
+      enable = true;
+ #     enableGnomeExtensions = true;
+      languagePacks = [ "sv-SE" ];
+#      /* ---- EXTENSIONS ---- */
+#      # Check about:support for extension/add-on ID strings.
+#      # Valid strings for installation_mode are "allowed", "blocked",
+#      # "force_installed" and "normal_installed".
+#      ExtensionSettings = {
+#        
+#      };    
+    };
+  };
+ 
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    gedit
+    wget
+    efibootmgr
+    bitwarden
+    neofetch
+    gitFull
+    #GPG
+    age
+    sops
+    gnupg
+    pinentry
+    pinentry-curses
+    #
+ ];
+ 
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+#    pinentryFlavor = "curses";
+    enableSSHSupport = true;
+  };
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  #configure SSH
+  services.openssh = {
+    enable = true;
+    settings = {
+      X11Forwarding = true;
+      PermitRootLogin = "no"; # disable root login
+      PasswordAuthentication = true; # disable password login
+    };
+    openFirewall = true;
+  };
+
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
+
