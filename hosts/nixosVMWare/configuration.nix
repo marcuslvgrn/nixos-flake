01ee0b256acc666ffe6db262cf6953d0d177e014
marcuslvgrn <marcuslvgrn@gmail.com>
2025-07-13 13:21:23 +0200

    Corrected hostname

--- hosts/nixosVMWare/configuration.nix
+++ hosts/nixosVMWare/configuration.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, home-manager, ... }:
+{ config, lib, pkgs, home-manager, hostname, ... }:
 
 {
   imports = [
@@ -18,7 +18,7 @@
     ../../common/sops.nix
   ];
 
-  networking.hostName = "nixosVMWare";
+  networking.hostName = hostname;
 
   swapDevices = [{
     device = "/swap/swapfile";
