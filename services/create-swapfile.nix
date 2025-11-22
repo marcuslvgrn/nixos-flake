01bea1be8cbdc95b36efcf5112a23b6d9e1fe88b
marcuslvgrn <marcuslvgrn@gmail.com>
2025-11-22 21:12:58 +0100

    Bugfix in create-swapfile service

--- services/create-swapfile.nix
+++ services/create-swapfile.nix
@@ -10,6 +10,7 @@
         if [[ -f "$swapfile" ]]; then
           echo "Swap file $swapfile already exists, taking no action"
         else
+          /run/current-system/sw/bin/mkdir -p /swap
           ${pkgs.coreutils}/bin/truncate -s 0 /swap/swapfile
           ${pkgs.e2fsprogs}/bin/chattr +C /swap/swapfile
         fi
