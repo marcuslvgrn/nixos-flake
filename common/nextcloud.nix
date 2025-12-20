e66395a93352eb8ce789097440c78199e4b16bef
marcuslvgrn <marcuslvgrn@gmail.com>
2025-12-20 20:01:44 +0100

    Whiteboard for nextcloud

--- common/nextcloud.nix
+++ common/nextcloud.nix
@@ -76,6 +76,16 @@
   services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
     forceSSL = true;
     enableACME = true;
+    locations."/whiteboard/" = {
+      extraConfig = ''
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host $host;
+        proxy_pass http://localhost:3002/;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+      '';
+    };
   };
 
   ##########################
